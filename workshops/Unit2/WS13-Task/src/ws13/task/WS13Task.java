af01006ad92e799df1c716ab1605a4530d47093f
cmportero1 <porteris123@gmail.com>
2021-02-19 16:40:18 -0500

    HW14ARRAYS

--- workshops/Unit2/WS13-Task/src/ws13/task/WS13Task.java
+++ workshops/Unit2/WS13-Task/src/ws13/task/WS13Task.java
@@ -22,48 +22,48 @@ public class WS13Task {
   Scanner input = new Scanner(System.in);
   int opcion=0;
   do{
-  System.out.println(" ====WELCOME====");
-  System.out.println(" ====My nickname is beau====");
-  System.out.println(" ======== OPTION MENU ========= ");
+  System.out.println(" WELCOME");
+  System.out.println(" My nickname is beau");
+  System.out.println(" OPTION MENU  ");
   System.out.println(" _____________ ");
   System.out.println(" _____________ ");
-  System.out.println(" ======== 1. Calculator for the area of ​​the Pentagon ========= ");
-  System.out.println(" ======== 2. Calculator for the area of ​​the Rhomboid========= ");
-  System.out.println(" ======== 3. Calculator for the area of ​​the Trapezoid========= ");
+  System.out.println(" 1. Calculator for the area of ​​the Pentagon  ");
+  System.out.println(" 2. Calculator for the area of ​​the Rhomboid ");
+  System.out.println(" 3. Calculator for the area of ​​the Trapezoid");
   System.out.println(" _____________ ");
   System.out.println(" _____________ ");
-  System.out.println(" ====Enter an option====");
+  System.out.println(" Enter an option");
   opcion = input.nextInt();
   switch (opcion){
   
    
       
         case 1:
-         System.out.println("===Enter the perimeter===");
+         System.out.println("Enter the perimeter");
          int perimetro = input.nextInt();
-         System.out.println("===Enter the Apothem===");
+         System.out.println("Enter the Apothem");
          int apothem= input.nextInt();
-         System.out.println("===THE PENTAGON AREA IS=== :"+pentagonarea(perimetro,apothem));
+         System.out.println("THE PENTAGON AREA IS :"+pentagonarea(perimetro,apothem));
         break;
         case 2:
-         System.out.println("===Enter base===");
+         System.out.println("Enter base");
          int base_r =input.nextInt();
-         System.out.println("===Enter Height===");
+         System.out.println("Enter Height");
          int height_r =input.nextInt();
-         System.out.println("===THE ROMBOID AREA IS===:"+rhomboidarea(base_r,height_r));
+         System.out.println("THE ROMBOID AREA IS:"+rhomboidarea(base_r,height_r));
         break;
         case 3:
-         System.out.println("===ENTER MAJOR BASE===");
+         System.out.println("ENTER MAJOR BASE");
          int base_ma=input.nextInt();
-         System.out.println("===ENTER THE MINOR BASE===");
+         System.out.println("ENTER THE MINOR BASE");
          int base_me=input.nextInt();
-         System.out.println("===ENTER HEIGHT===");
+         System.out.println("ENTER HEIGHT");
          int height_t=input.nextInt();
-         System.out.println("===THE AREA OF THE TRAPEZE IS===:"+trapeziumarea(base_ma,base_me,height_t));
+         System.out.println("THE AREA OF THE TRAPEZE IS:"+trapeziumarea(base_ma,base_me,height_t));
         break;
 
   }
-        System.out.println(" ====I hope it could help you====");
+        System.out.println(" I hope it could help you");
   }while(opcion!=3);
  }
 
